fe9917c2fafd34a0b42685e3e9d1e96152293aba
Hung An Kuo <andykuo8766@gapp.nthu.edu.tw>
2021-09-19 15:03:48 +0800

    Upload Lab5

---
+++ Random/main.c
@@ -0,0 +1,46 @@
+#include "uart.h"
+#include "mbox.h"
+#include "rand.h"
+
+void main()
+{
+    // set up serial console
+    uart_init();
+
+    // set up random number generator
+    rand_init();  
+    
+    uart_puts("Here goes a random number: ");
+    uart_hex(rand(0,4294967295));
+    uart_puts("\n");
+
+    // get the board's unique serial number with a mailbox call
+    mbox[0] = 8*4;                  // length of the message
+    mbox[1] = MBOX_REQUEST;         // this is a request message
+    
+    mbox[2] = MBOX_TAG_GETSERIAL;   // get serial number command
+    mbox[3] = 8;                    // buffer size
+    mbox[4] = 8;
+    mbox[5] = 0;                    // clear output buffer
+    mbox[6] = 0;
+
+    mbox[7] = MBOX_TAG_LAST;
+
+    // send the message to the GPU and receive answer
+    if (mbox_call(MBOX_CH_PROP)) {
+        uart_puts("My serial number is: ");
+        uart_hex(mbox[6]);
+        uart_hex(mbox[5]);
+        uart_puts("\n");
+    } else {
+        uart_puts("Unable to query serial!\n");
+    }
+
+    // echo everything back
+    while(1) {
+        uart_send(uart_getc());
+    }
+
+
+
+}
