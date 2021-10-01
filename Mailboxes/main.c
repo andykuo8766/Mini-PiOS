ae8069dda58b97db336396792e24b8e85465230d
Hung An Kuo <andykuo8766@gapp.nthu.edu.tw>
2021-10-01 23:48:17 +0800

    Update Lab3

--- Mailboxes/main.c
+++ Mailboxes/main.c
@@ -3,6 +3,20 @@
 
 void main()
 {
+    /*
+    //Lab 2: UART1
+    // set up serial console
+    uart_init();
+    
+    // say hello
+    uart_puts("Hello World!\n");
+    
+    // echo everything back
+    while(1) {
+        uart_send(uart_getc());
+    }
+    */
+    
     //Lab 3: Mailboxes
     // set up serial console
     uart_init();
