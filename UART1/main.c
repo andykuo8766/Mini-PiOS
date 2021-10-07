34c450fa42cb7a648345337f64e5b10324edb6c9
Hung An Kuo <andykuo8766@gapp.nthu.edu.tw>
2021-10-07 23:49:15 +0800

    Update main.c

--- UART1/main.c
+++ UART1/main.c
@@ -1,7 +1,13 @@
+#define Lab 2
+
 #include "uart.h"
 
 void main()
 {
+#if Lab == 1
+    //Lab 1: Multicore C
+    while(1);
+#elif Lab == 2
     //Lab 2: UART1
     // set up serial console
     uart_init();
@@ -13,4 +19,5 @@ void main()
     while(1) {
         uart_send(uart_getc());
     }
+#endif
 }
