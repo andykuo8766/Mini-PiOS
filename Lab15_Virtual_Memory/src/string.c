6129d1df2e0b338078f29f2bd57a3710ffe184aa
andykuo8766 <andykuo8766@gapp.nthu.edu.tw>
2021-11-15 06:36:02 -0800

    Upload Lab15

---
+++ Lab15_Virtual_Memory/src/string.c
@@ -0,0 +1,27 @@
+#include "string.h"
+
+int strcmp(const char *cs, const char *ct)
+{
+	unsigned char c1, c2;
+
+	while (1) {
+		c1 = *cs++;
+		c2 = *ct++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+	}
+	return 0;
+}
+
+int memcmp(const void *cs, const void *ct, size_t count)
+{
+	const unsigned char *su1, *su2;
+	int res = 0;
+
+	for (su1 = cs, su2 = ct; 0 < count; ++su1, ++su2, count--)
+		if ((res = *su1 - *su2) != 0)
+			break;
+	return res;
+}
