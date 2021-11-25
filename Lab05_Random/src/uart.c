c652509b02f252dfd98732e259a2e65d8776859c
andykuo8766 <andykuo8766@gapp.nthu.edu.tw>
2021-11-25 05:00:01 -0800

    Update uart.c mbox.h

--- Lab05_Random/src/uart.c
+++ Lab05_Random/src/uart.c
@@ -1,5 +1,11 @@
-#include "gpio.h"
+#define Lab 3
 
+
+#include "gpio.h"
+#include "mbox.h"
+
+
+#if Lab < 4
 /* Auxilary mini UART registers */
 #define AUX_ENABLE      ((volatile unsigned int*)(MMIO_BASE+0x00215004))
 #define AUX_MU_IO       ((volatile unsigned int*)(MMIO_BASE+0x00215040))
@@ -13,6 +19,24 @@
 #define AUX_MU_CNTL     ((volatile unsigned int*)(MMIO_BASE+0x00215060))
 #define AUX_MU_STAT     ((volatile unsigned int*)(MMIO_BASE+0x00215064))
 #define AUX_MU_BAUD     ((volatile unsigned int*)(MMIO_BASE+0x00215068))
+
+#elif Lab >= 4
+
+/* PL011 UART registers */
+#define UART0_DR        ((volatile unsigned int*)(MMIO_BASE+0x00201000))
+#define UART0_FR        ((volatile unsigned int*)(MMIO_BASE+0x00201018))
+#define UART0_IBRD      ((volatile unsigned int*)(MMIO_BASE+0x00201024))
+#define UART0_FBRD      ((volatile unsigned int*)(MMIO_BASE+0x00201028))
+#define UART0_LCRH      ((volatile unsigned int*)(MMIO_BASE+0x0020102C))
+#define UART0_CR        ((volatile unsigned int*)(MMIO_BASE+0x00201030))
+#define UART0_IMSC      ((volatile unsigned int*)(MMIO_BASE+0x00201038))
+#define UART0_ICR       ((volatile unsigned int*)(MMIO_BASE+0x00201044))
+
+#endif
+
+
+
+#if Lab < 4
 
 /**
  * Set baud rate and characteristics (115200 8N1) and map to GPIO
@@ -42,6 +66,52 @@ void uart_init()
     *AUX_MU_CNTL = 3;      // enable Tx, Rx
 }
 
+#elif Lab >= 4
+
+/**
+ * Set baud rate and characteristics (115200 8N1) and map to GPIO
+ */
+void uart_init()
+{
+    register unsigned int r;
+
+    /* initialize UART */
+    *UART0_CR = 0;         // turn off UART0
+
+    /* set up clock for consistent divisor values */
+    mbox[0] = 9*4;
+    mbox[1] = MBOX_REQUEST;
+    mbox[2] = MBOX_TAG_SETCLKRATE; // set clock rate
+    mbox[3] = 12;
+    mbox[4] = 8;
+    mbox[5] = 2;           // UART clock
+    mbox[6] = 4000000;     // 4Mhz
+    mbox[7] = 0;           // clear turbo
+    mbox[8] = MBOX_TAG_LAST;
+    mbox_call(MBOX_CH_PROP);
+
+    /* map UART0 to GPIO pins */
+    r=*GPFSEL1;
+    r&=~((7<<12)|(7<<15)); // gpio14, gpio15
+    r|=(4<<12)|(4<<15);    // alt0
+    *GPFSEL1 = r;
+    *GPPUD = 0;            // enable pins 14 and 15
+    r=150; while(r--) { asm volatile("nop"); }
+    *GPPUDCLK0 = (1<<14)|(1<<15);
+    r=150; while(r--) { asm volatile("nop"); }
+    *GPPUDCLK0 = 0;        // flush GPIO setup
+
+    *UART0_ICR = 0x7FF;    // clear interrupts
+    *UART0_IBRD = 2;       // 115200 baud
+    *UART0_FBRD = 0xB;
+    *UART0_LCRH = 0x7<<4;  // 8n1, enable FIFOs
+    *UART0_CR = 0x301;     // enable Tx, Rx, UART
+}
+
+#endif
+
+
+#if Lab < 4
 /**
  * Send a character
  */
@@ -52,6 +122,21 @@ void uart_send(unsigned int c) {
     *AUX_MU_IO=c;
 }
 
+#elif Lab >= 4
+
+/**
+ * Send a character
+ */
+void uart_send(unsigned int c) {
+    /* wait until we can send */
+    do{asm volatile("nop");}while(*UART0_FR&0x20);
+    /* write the character to the buffer */
+    *UART0_DR=c;
+}
+
+#endif
+
+#if Lab < 4
 /**
  * Receive a character
  */
@@ -64,6 +149,23 @@ char uart_getc() {
     /* convert carrige return to newline */
     return r=='\r'?'\n':r;
 }
+
+#elif Lab >= 4
+
+/**
+ * Receive a character
+ */
+char uart_getc() {
+    char r;
+    /* wait until something is in the buffer */
+    do{asm volatile("nop");}while(*UART0_FR&0x10);
+    /* read it and return */
+    r=(char)(*UART0_DR);
+    /* convert carrige return to newline */
+    return r=='\r'?'\n':r;
+}
+
+#endif
 
 /**
  * Display a string
